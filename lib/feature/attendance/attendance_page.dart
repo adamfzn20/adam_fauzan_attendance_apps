86e505564930ceefb9daae99d825cf5b74396fab
Adam Fauzan <adamfzan@gmail.com>
2024-08-01 16:04:57 +0700

    attendance feature

--- lib/feature/attendance/attendance_page.dart
+++ lib/feature/attendance/attendance_page.dart
@@ -1,12 +1,64 @@
+import 'package:adam_fauzan_attendance_apps/feature/attendance/attendance_controller.dart';
+import 'package:adam_fauzan_attendance_apps/utills/widget/app_bar/app_bar_widget.dart';
+import 'package:adam_fauzan_attendance_apps/utills/widget/button/primary_button.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AttendancePage extends StatelessWidget {
   const AttendancePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Masuk")),
-    );
+    return GetBuilder<AttendanceController>(builder: (controller) {
+      return Scaffold(
+        appBar: AppBarWidget.secondaryAppbar(
+            titleString: "txt_live_attendance_live_attendance".tr,
+            context: context),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                child: Obx(() {
+                  return GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(
+                        controller.userLocation.value!.latitude,
+                        controller.userLocation.value!.longitude,
+                      ),
+                      zoom: 15,
+                    ),
+                    markers: Set<Marker>.of(controller.markers.values),
+                  );
+                }),
+              ),
+              Center(
+                  child: PrimaryButton(
+                onPressed: controller.checkIn,
+                title: 'Absent',
+              )),
+              Obx(() {
+                if (controller.attendances.isEmpty) {
+                  return Text("No attendance records found.");
+                }
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: controller.attendances.length,
+                  itemBuilder: (context, index) {
+                    final attendance = controller.attendances[index];
+                    return ListTile(
+                      title: Text('Attendance: ${attendance['timestamp']}'),
+                      subtitle: Text('Location: ${attendance['location']}'),
+                    );
+                  },
+                );
+              }),
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
+import geolocator_apple
 import just_audio
 import package_info_plus
 import path_provider_foundation
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
